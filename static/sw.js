cfbb33d5214932bea34de585a3de4b9ff4997557
kiki48boutique <kiki48.boutique@gmail.com>
2026-08-12 19:56:09 +0200

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -9,8 +9,14 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
 });
 
-// Gestion des requêtes (passe toujours par le réseau pour garder vos prix/decks à jour)
+// Gestion des requêtes
 self.addEventListener('fetch', (event) => {
+  // Ignorer les requêtes non-GET (ex: POST pour la connexion) et les appels d'API
+  if (event.request.method !== 'GET' || event.request.url.includes('/api/')) {
+    return; // Laisse le navigateur gérer la requête directement
+  }
+
+  // Utilisation du réseau en priorité avec fallback sur le cache pour le reste (GET)
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
